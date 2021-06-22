c5f3b32feac3a05c06ea5aa193adaadf7ff834e8
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-22 08:28:05 -0700

    Added

--- LineComparison.java
+++ LineComparison.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 class LineComparison
 {
   public static void main(String[] args)
  {
    System.out.println("welcome to line comparison computation");
-  }
+   Scanner sc = new Scanner(System.in);
+		System.out.println("Enter co-ordinate x1 : ");
+		double x1=sc.nextDouble();
+		System.out.println("Enter co-ordinate y1 : ");
+		double y1=sc.nextDouble();
+		System.out.println("Enter co-ordinate x2 : ");
+		double x2=sc.nextDouble();
+		System.out.println("Enter co-ordinate y2 : ");
+		double y2=sc.nextDouble();
+
+		double Lenght = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		System.out.println("Length of Line : "+Lenght) ;
+
+
+   }
  }
 
